45a1c25c6339d12726f753c763db531a540e60a3
Matthew Ortiz <mortiz13@csuchico.edu>
2026-04-15 11:40:16 -0700

    Work in progress

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
 
 using std::string;
 
@@ -23,6 +24,23 @@ int Password::count_leading_characters(string phrase){
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+
+// Implementation E
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
   return false;
+}
+
+unsigned int unique_characters(string){
+
+  
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,42 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, two_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZZ");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, mixed_case_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+
+TEST(PasswordTest, mixed_case_spaces_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("Z Zz"));
+}
+
+TEST(PasswordTest, mixed_case_starting_space_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters(" ZZz"));
+}
+
+TEST(PasswordTest, mixed_case_pass)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters("zZZz"));
+}
+
+
